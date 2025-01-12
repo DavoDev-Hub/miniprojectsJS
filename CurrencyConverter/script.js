e40db454d581b9900a03f1417045f863f645cc1e
DavoDev-Hub <juanpajmz13@gmail.com>
2025-01-11 23:41:01 -0600

    I start with a javascript file

--- CurrencyConverter/script.js
+++ CurrencyConverter/script.js
@@ -1,1 +1,18 @@
 // Here we add a logic
+//
+//
+
+let from = document.getElementById("from-currency").value;
+let to = document.getElementById("to-currency").value;
+let resultado;
+
+switch (expresión) {
+  case 'USD':
+    break;
+  case 'EUR':
+    break;
+  case 'GBP':
+    break;
+  default:
+}
+
